e1dc0c4907666abf4864f8a007ed8d6e2732499f
Shendul <XiranSonja@Gmail.com>
2018-01-02 17:13:35 -0800

    Day 11 Part one done in Java

---
+++ Advent of Code/src/Day11.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class Day11 {
+
+	public static void main(String[] args) {
+		
+		// init endpoint x, y, and z scores.
+		int endX = 0, endY = 0, endZ = 0;
+		
+		// scan in the moves
+		Scanner in = new Scanner(System.in);
+		String line = in.nextLine();
+		String[] moves = line.split(",");
+		
+		for (int m = 0; m < moves.length; m++) {
+			
+			if (moves[m].equals("n")) {
+				endZ--;
+				endY++;
+			} else if (moves[m].equals("nw")) {
+				endY++;
+				endX--;
+			} else if (moves[m].equals("sw")) {
+				endX--;
+				endZ++;
+			} else if (moves[m].equals("s")) {
+				endY--;
+				endZ++;
+			} else if (moves[m].equals("se")) {
+				endY--;
+				endX++;
+			} else if (moves[m].equals("ne")) {
+				endX++;
+				endZ--;
+			} else {
+				System.out.println("Error: invalid move was made?");
+			}
+			
+		}
+		
+		// Take the maximum of the three scores absolute values and that is your distance
+		System.out.println(Math.max(Math.max(Math.abs(endX),Math.abs(endY)),Math.abs(endZ)));
+
+	}
+
+}
