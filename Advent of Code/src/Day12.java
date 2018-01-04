ee7cc93bd2c70ab77e0f8388df03762f3f1cebd3
Shendul <XiranSonja@Gmail.com>
2018-01-04 12:07:20 -0700

    Rewrote and completed part one day 12 in java

--- Advent of Code/src/Day12.java
+++ Advent of Code/src/Day12.java
@@ -1,47 +1,72 @@
-import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.Scanner;
 
 public class Day12 {
 
+	public static final int MAX_VALUE = 100000;
+	
 	public static void main(String[] args) {
-		
-		// init an arraylist to hold all of the numbers
-		ArrayList<Integer> group = new ArrayList<Integer>();
-		group.add(0);
-		System.out.println(group);
+		LinkedList<Integer> adjList[] = new LinkedList[MAX_VALUE];  
 		
 		Scanner in = new Scanner(System.in);
+		int index = 0;
+		boolean scanning = true;
 		
-		while(in.hasNextLine()) {
-			String line = in.nextLine();
-			String[] split = line.split(" <-> ");
+		while(scanning) {
 			
-			if (group.contains(Integer.parseInt(split[0]))) {
-				//System.out.println("group: " + group);
-				//System.out.println("group contains: " + split[0]);
-				
-				String[] newConnections = split[1].split(", ");
-				for (int c = 0; c < newConnections.length; c++) {
-					Integer newConnection = Integer.parseInt(newConnections[c]);
-					if (!group.contains(newConnection))
-						group.add(newConnection);
-				}
-			} else {
-				//System.out.println("group does not contain this number");
-				// check reverse side
-				String[] newConnections = split[1].split(", ");
-				for (int c = 0; c < newConnections.length; c++) {
-					Integer newConnection = Integer.parseInt(newConnections[c]);
-					if (group.contains(newConnection))
-						group.add(Integer.parseInt(split[0]));
-				}
+			adjList[index] = new LinkedList<>();
+			String line = in.nextLine();
+			if (line.equals("exit")) {
+				scanning = false;
+				break;
 			}
-			System.out.println("group: " + group);
-			System.out.println("num programs: " + group.size());
+			String[] split = line.split(" <-> ");
+			String[] connections = split[1].split(", ");
+			for (int i = 0; i < connections.length; i++) {
+				adjList[index].add(Integer.parseInt(connections[i]));
+			}
+			//System.out.println(Arrays.toString(adjList));
+			index++;
 			
 		}
+		System.out.println(BFS(adjList));
 		
 
+	}
+	
+	public static int BFS(LinkedList<Integer> adjList[]) {
+		int programs = 0;
+		int start = 0;
+		
+		boolean visited[] = new boolean[MAX_VALUE];
+		
+		// Create a queue for BFS
+        LinkedList<Integer> queue = new LinkedList<Integer>();
+        
+        visited[start]= true;
+        queue.add(start);
+        
+        while (queue.size() != 0)
+        {
+            // Dequeue a vertex from queue and increase programs by one
+            start = queue.poll();
+            programs++;
+ 
+            Iterator<Integer> i = adjList[start].listIterator();
+            while (i.hasNext())
+            {
+                int num = i.next();
+                if (!visited[num])
+                {
+                    visited[num] = true;
+                    queue.add(num);
+                }
+            }
+        }
+		
+		return programs;
 	}
 
 }
